4d95fa2012f600a4be9299f689c7a81e217fea7f
cuong1999 <chicuong1999@gmail.com>
2023-01-09 16:16:30 +0700

    up

--- src/util/config.tsx
+++ src/util/config.tsx
@@ -55,9 +55,9 @@ export const {
   USER_LOGIN,
 } = config;
 
-const DOMAIN = "https://shop.cyberlearn.vn/api";
+const DOMAIN = "https://fiverrnew.cybersoft.edu.vn/api";
 const TOKEN_CYBERSOFT =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJKYXZhIDE3IiwiSGV0SGFuU3RyaW5nIjoiMTkvMTIvMjAyMiIsIkhldEhhblRpbWUiOiIxNjcxNDA4MDAwMDAwIiwibmJmIjoxNjQ4NjU5NjAwLCJleHAiOjE2NzE1NTU2MDB9.OhFEeK7ExgP3U24hEq7GxmL5VzAjzaEPPeOZpaWzZGE";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCAzNUUiLCJIZXRIYW5TdHJpbmciOiIwNy8wNi8yMDIzIiwiSGV0SGFuVGltZSI6IjE2ODYwOTYwMDAwMDAiLCJuYmYiOjE2NTczODYwMDAsImV4cCI6MTY4NjI0MzYwMH0.XsCcIZvawxcwye8KVYB2vJK4d3Gbr1XROtNyAL8nypA";
 
 /* Cấu hình request cho tất cả api - response cho tất cả kết quả từ api trả về */
 
